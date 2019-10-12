93c77bed0388308957b4358ecbfa1e81841cf737
honki <276086064@qq.com>
2019-10-12 10:15:10 +0800

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">聊天室</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
